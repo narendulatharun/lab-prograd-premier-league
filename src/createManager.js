857d4f254b651162ff66de83df7a51cf7086d0cd
narendulatharun <42996599+narendulatharun@users.noreply.github.com>
2020-03-16 17:04:47 +0530

    Add files via upload

---
+++ src/createManager.js
@@ -0,0 +1,5 @@
+function createManager(managerName, managerAge, currentTeam, trophiesWon) {
+  let manager = [];
+  manager.push(managerName, managerAge, currentTeam, trophiesWon);
+  return manager;
+}
